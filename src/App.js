3a04a5a1171061d8966d9d9c2eb249e08d3708e5
Si Hyeong Lee <super2451894@gmail.com>
2022-05-15 23:22:25 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Landing from "./pages/landing";
+
+const App = () => {
+  return (
+    <div>
+      <Landing></Landing>
+    </div>
+  );
+};
+
+export default App;
