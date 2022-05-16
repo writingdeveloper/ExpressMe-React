b57cdc8f87a5d2871f65c610057520d3fb0400d3
Si Hyeong Lee <super2451894@gmail.com>
2022-05-16 16:01:30 +0900

    Change main title
    - Change main title in main page

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -106,7 +106,7 @@ const Nav = () => {
           justifyContent="space-between"
         >
           <Link href="/">
-            <a className={styles.logo}>Next.js MongoDB App</a>
+            <a className={styles.logo}>ExpressMe</a>
           </Link>
           <Container>
             {user ? (
