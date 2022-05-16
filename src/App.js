68043a32f1fe00dc8a37756a349e0d173826ac99
Si Hyeong Lee <super2451894@gmail.com>
2022-05-16 10:56:44 +0900

    Project Initial Commit V2 MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import Landing from "./pages/Landing";
 
-const App = () => {
-  return (
-    <div>
-      <Landing></Landing>
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
