47dd85018d3a17d2ce1c08b7b25a92732b7040c7
Si Hyeong Lee <super2451894@gmail.com>
2022-05-16 11:31:53 +0900

    Remove useless navbar part

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,18 +18,6 @@ const Navbar = () => {
           <p>
             <a href="#home">Home</a>
           </p>
-          <p>
-            <a href="#wgpt3">What is GPT3?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
